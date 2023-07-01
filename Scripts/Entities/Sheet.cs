b10723e397597ea78b395307c852151d3ea38fb0
Dylan <109418123+WebbD777@users.noreply.github.com>
2023-07-01 22:16:59 +0200

    Managed to draw snake body and head to window

--- Scripts/Entities/Sheet.cs
+++ Scripts/Entities/Sheet.cs
@@ -25,6 +25,8 @@ namespace SnakeGame.Scripts.Entities
         private int rowPellet;
         private int colPellet;
 
+        Rectangle destinationRectangle; 
+
         private List<Microsoft.Xna.Framework.Rectangle> arrSheet;
 
         public Vector2 enemyBounds = new Vector2(300, 150);
@@ -33,6 +35,8 @@ namespace SnakeGame.Scripts.Entities
             _texture = texture;
             _row = row;
             _col = coloumn;
+
+            arrSheet = new List<Rectangle>();
         }
 
         public void LoadContent() {
@@ -49,6 +53,20 @@ namespace SnakeGame.Scripts.Entities
              rowPellet = 2 / _col;
              colPellet = 2 % _col; //Pellet
 
+            destinationRectangle = new Rectangle((int)enemyBounds.X, (int)enemyBounds.Y, width, height);
+            
+            // Add the body, head, pellet source recyangles to list
+            // Lastly the destination rectangle
+        //    Rectangle arrBod = new Rectangle(0, 0, 32, 32); arrSheet.Add(arrBod);
+            arrSheet.Add(new Rectangle(width * colBody, height * rowBody, width, height));
+            arrSheet.Add(new Rectangle(width * colHead, height * rowHead, width, height));
+            arrSheet.Add(new Rectangle(width * colPellet, height * rowPellet, width, height));
+            arrSheet.Add(destinationRectangle);
+        }
+
+        public List<Rectangle> getListSprite()
+        {
+            return arrSheet;
         }
 
         public void Update(SpriteBatch spriteBatch, GraphicsDevice graphics) 
@@ -60,17 +78,19 @@ namespace SnakeGame.Scripts.Entities
 
         public void Draw(SpriteBatch spriteBatch) {
 
+            /*
             Rectangle sourceRectangle = new Rectangle(0, 0, 0, 0);
             Rectangle destinationRectangle = new Rectangle(0, 0, 0, 0);
 
             sourceRectangle.X = width * colHead; sourceRectangle.Y = height * rowHead; sourceRectangle.Width = width; sourceRectangle.Height = height;
-            destinationRectangle.X = (int)enemyBounds.X; destinationRectangle.Y = (int)enemyBounds.Y; destinationRectangle.Width = width; destinationRectangle.Height = height;
+     //       destinationRectangle.X = (int)enemyBounds.X; destinationRectangle.Y = (int)enemyBounds.Y; destinationRectangle.Width = width; destinationRectangle.Height = height;
 
             spriteBatch.Begin();
 
             spriteBatch.Draw(_texture, destinationRectangle, sourceRectangle, Color.White);
 
             spriteBatch.End();
+            */
 
 
         }
--- Scripts/States/GameState.cs
+++ Scripts/States/GameState.cs
@@ -13,26 +13,48 @@ namespace SnakeGame.Scripts.States
     internal class GameState : State
     {
         private Sheet sheet;
+        bool loadedBol; // To stop reloading snake sprite sheet
+        private List<Rectangle> sheetArr;
+        private Texture2D sheetSprite;
         public GameState(Game1 game, GraphicsDevice graphicsDevice, ContentManager contentManager) : base(game, graphicsDevice, contentManager)
         {
+            loadedBol = false;
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            // throw new NotImplementedException();
-            var sheetSprite = _content.Load<Texture2D>("Sprites/snake");
-            sheet = new Sheet(sheetSprite, 1, 3);
-            sheet.LoadContent();
+            
+            if (loadedBol == false) // To stop reloading snake sprite sheet
+                LoadContent();
 
-            sheet.Draw(spriteBatch);
+            // 
+            sheetArr = sheet.getListSprite();
+            Rectangle destRec = sheetArr[3];
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(sheetSprite, new Rectangle(destRec.X, destRec.Y, destRec.Width, destRec.Height), sheetArr[0], Color.White);
+            spriteBatch.Draw(sheetSprite, new Rectangle(destRec.X + 33, destRec.Y, destRec.Width, destRec.Height), sheetArr[1], Color.White);
+
+            spriteBatch.End();
+
+            //  sheet.Draw(spriteBatch);
         }
 
         public override void LoadContent()
         {
             //    throw new NotImplementedException();
-            
 
-            
+             // To stop reloading snake sprite sheet
+           // {
+                sheetSprite = _content.Load<Texture2D>("Sprites/snake");
+                sheet = new Sheet(sheetSprite, 1, 3);
+                sheet.LoadContent();
+
+                loadedBol = true;
+          //  }
+
+
         }
 
         public override void LoadGameButton_Click(object sender, EventHandler e)
